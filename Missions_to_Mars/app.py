75e7ebcc228c4632381bb16a085eaf60b2285420
Divya Shetty <divyashettyk@gmail.com>
2021-03-27 08:15:21 -0500

    app.py updated

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars as ms
+
+# create instance of Flask app
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# create a main route that renders index.html template
+@app.route("/")
+def index():
+    mars_news = mongo.db.news.find_one()
+
+    return render_template("index.html", mars_news= mars_news)
+
+
+@app.route("/scrape")
+def scraper():
+    # create a collection
+    news = mongo.db.news
+    # get data fromt the scrape_mars.py file
+    mars_data = ms.scrape()
+    # Update Mongo collection with new data 
+    # Note: {} - update all upsert- insert if not there or update entire collection if there
+    news.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
